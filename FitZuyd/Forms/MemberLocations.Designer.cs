d4970649e5bd62d88a45edf37f4ade77f11bf0ef
Nan Ferdinandus <94622571+NanFerdinandus@users.noreply.github.com>
2024-04-16 11:49:55 +0200

    test

--- FitZuyd/Forms/MemberLocations.Designer.cs
+++ FitZuyd/Forms/MemberLocations.Designer.cs
@@ -40,9 +40,10 @@
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(625, 378);
+            this.btnClose.Location = new System.Drawing.Point(938, 591);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(100, 32);
+            this.btnClose.Size = new System.Drawing.Size(150, 50);
             this.btnClose.TabIndex = 18;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -51,9 +52,10 @@
             // btnDetails
             // 
             this.btnDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDetails.Location = new System.Drawing.Point(625, 112);
+            this.btnDetails.Location = new System.Drawing.Point(938, 175);
+            this.btnDetails.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnDetails.Name = "btnDetails";
-            this.btnDetails.Size = new System.Drawing.Size(100, 32);
+            this.btnDetails.Size = new System.Drawing.Size(150, 50);
             this.btnDetails.TabIndex = 16;
             this.btnDetails.Text = "Details";
             this.btnDetails.UseVisualStyleBackColor = true;
@@ -63,19 +65,19 @@
             this.labelMemberText.AutoSize = true;
             this.labelMemberText.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelMemberText.ForeColor = System.Drawing.Color.White;
-            this.labelMemberText.Location = new System.Drawing.Point(8, 37);
-            this.labelMemberText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelMemberText.Location = new System.Drawing.Point(12, 58);
             this.labelMemberText.Name = "labelMemberText";
-            this.labelMemberText.Size = new System.Drawing.Size(130, 35);
+            this.labelMemberText.Size = new System.Drawing.Size(210, 56);
             this.labelMemberText.TabIndex = 15;
             this.labelMemberText.Text = "Member";
             // 
             // btnLocations
             // 
             this.btnLocations.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLocations.Location = new System.Drawing.Point(489, 45);
+            this.btnLocations.Location = new System.Drawing.Point(734, 70);
+            this.btnLocations.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnLocations.Name = "btnLocations";
-            this.btnLocations.Size = new System.Drawing.Size(100, 32);
+            this.btnLocations.Size = new System.Drawing.Size(150, 50);
             this.btnLocations.TabIndex = 14;
             this.btnLocations.Text = "Locations";
             this.btnLocations.UseVisualStyleBackColor = true;
@@ -84,9 +86,10 @@
             // btnActivities
             // 
             this.btnActivities.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnActivities.Location = new System.Drawing.Point(625, 45);
+            this.btnActivities.Location = new System.Drawing.Point(938, 70);
+            this.btnActivities.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnActivities.Name = "btnActivities";
-            this.btnActivities.Size = new System.Drawing.Size(100, 32);
+            this.btnActivities.Size = new System.Drawing.Size(150, 50);
             this.btnActivities.TabIndex = 13;
             this.btnActivities.Text = "Activities";
             this.btnActivities.UseVisualStyleBackColor = true;
@@ -95,18 +98,19 @@
             // listViewMLocation
             // 
             this.listViewMLocation.HideSelection = false;
-            this.listViewMLocation.Location = new System.Drawing.Point(15, 112);
+            this.listViewMLocation.Location = new System.Drawing.Point(22, 175);
+            this.listViewMLocation.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listViewMLocation.Name = "listViewMLocation";
-            this.listViewMLocation.Size = new System.Drawing.Size(576, 300);
+            this.listViewMLocation.Size = new System.Drawing.Size(862, 466);
             this.listViewMLocation.TabIndex = 12;
             this.listViewMLocation.UseCompatibleStateImageBehavior = false;
             // 
             // MemberLocations
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.ClientSize = new System.Drawing.Size(733, 447);
+            this.ClientSize = new System.Drawing.Size(1100, 698);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnDetails);
             this.Controls.Add(this.labelMemberText);
@@ -114,7 +118,6 @@
             this.Controls.Add(this.btnActivities);
             this.Controls.Add(this.listViewMLocation);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MemberLocations";
             this.Text = "MemberLocations";
             this.ResumeLayout(false);
