fe5d58d75ae86b5b59e636a7aae0d11356723856
Nan Ferdinandus <94622571+NanFerdinandus@users.noreply.github.com>
2024-04-18 10:17:43 +0200

    MemberLocation btn Details added

--- FitZuyd/Forms/MemberLocations.cs
+++ FitZuyd/Forms/MemberLocations.cs
@@ -15,6 +15,7 @@ namespace FitZuyd.Forms
         public MemberLocations()
         {
             InitializeComponent();
+            btnDetails.Click += btnDetails_Click;
 
             Location location = new Location();
 
@@ -23,18 +24,15 @@ namespace FitZuyd.Forms
 
             listViewMLocation.Columns.Add("ID", 100);
             listViewMLocation.Columns.Add("Name: ", 150);
-            listViewMLocation.Columns.Add("Address: ", 200);
 
             foreach (DataRow row in location.GetAllLocations().Rows)
             {
                 string locationId = row["ID"].ToString();
                 string locationName = row["Name"].ToString();
-                string locationAddress = row["Address"].ToString();
 
                 ListViewItem listItem = new ListViewItem();
                 listItem.Text = locationId;
                 listItem.SubItems.Add(locationName);
-                listItem.SubItems.Add(locationAddress);
 
                 listViewMLocation.Items.Add(listItem);
             }
@@ -59,6 +57,25 @@ namespace FitZuyd.Forms
             MemberMenu memberMenu = new MemberMenu();
             memberMenu.Show();
             this.Hide();
+        }
+
+        private void btnDetails_Click(object sender, EventArgs e)
+        {
+            if (listViewMLocation.SelectedItems.Count > 0)
+            {
+                // Haal de geselecteerde locatie op
+                ListViewItem selectedItem = listViewMLocation.SelectedItems[0];
+
+                // Haal de adresinformatie op uit de geselecteerde locatie
+                string address = selectedItem.SubItems[2].Text;
+
+                // Toon het adres in een berichtvenster
+                MessageBox.Show($"Het adres van de geselecteerde locatie is: {address}", "Locatie Adres");
+            }
+            else
+            {
+                MessageBox.Show("Selecteer een locatie om het adres te tonen.", "Geen locatie geselecteerd");
+            }
         }
     }
 }
